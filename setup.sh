00b93658c9bda53670f820e4947703ed204a4a20
thomasmcg77 <66293154+thomasmcg77@users.noreply.github.com>
2023-02-25 16:18:30 -0600

    Testing setup script to install console launcher

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir lib
+
+cd lib
+
+wget https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.7.0/junit-platform-console-standalone-1.7.0-all.jar
+
+cd ..
